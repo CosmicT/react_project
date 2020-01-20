504a2e80bd5cf789e69e92519bde2125203397f1
Roy <riddhij@a483e7479a3e.ant.amazon.com>
2020-01-20 14:16:13 +0530

    pdf changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-/*import React from 'react';
+import React, { Component }  from 'react';
+import { Document, Page } from "react-pdf";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  /*return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,12 +21,50 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  );*/
+
+  state = { numPages: null, pageNumber: 1 };
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
+  goToPrevPage = () =>
+    this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
+  goToNextPage = () =>
+    this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
+
+  render() {
+    const { pageNumber, numPages } = this.state;
+
+    return (
+      <div>
+        <nav>
+          <button onClick={this.goToPrevPage}>Prev</button>
+          <button onClick={this.goToNextPage}>Next</button>
+        </nav>
+
+        <div style={{ width: 600 }}>
+          <Document
+            file="/example.pdf"
+            onLoadSuccess={this.onDocumentLoadSuccess}
+          >
+            <Page pageNumber={pageNumber} width={600} />
+          </Document>
+        </div>
+
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
+    );
+  }
+
 }
 
-export default App;*/
+export default App;
 
-import React, { Component } from "react";
+/*import React, { Component } from "react";
 import { Document, Page } from "react-pdf";
 
 export default class App extends Component {
@@ -66,4 +105,6 @@ export default class App extends Component {
     );
   }
 }
+
+*/
 
